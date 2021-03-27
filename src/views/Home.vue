f0f9ca1818e5852eb0199d6b8e3642c8efe02e03
Julian Sauer <julian_sauer@gmx.net>
2021-03-27 23:56:50 +0100

    Show latest data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <CurrentWeather/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import CurrentWeather from '@/components/CurrentWeather.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    CurrentWeather: CurrentWeather,
   },
 })
 export default class Home extends Vue {}
