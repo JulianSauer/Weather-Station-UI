a2dde51bf957b5051e9166571913b79e39341791
Julian Sauer <julian_sauer@gmx.net>
2021-04-02 02:18:30 +0200

    Button for manual update

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
     <v-app>
       <v-app-bar app color="teal" dark>
         <span class="mr-2">Weather Station</span>
+
         <v-spacer></v-spacer>
-        <span class="mr-2">{{ timestamp }}</span>
+
+        <span>{{ timestamp }}</span>
+        <v-btn v-on:click="updateSensorData24" icon>
+          <v-icon>mdi-cached</v-icon>
+        </v-btn>
       </v-app-bar>
 
       <v-main>
@@ -39,16 +44,26 @@ export default Vue.extend({
 
   mixins: [dateParser, weatherStationClient],
   created() {
-    let now = new Date()
-    let begin = this.formatDate(now, "YYYYMMDD-hhmmss")
-    now.setHours(now.getHours() - 10)
-    let end = this.formatDate(now, "YYYYMMDD-hhmmss")
-    this.updateStore(store, begin, end)
+    this.updateSensorData24()
 
     this.$store.subscribe((mutation, state) => {
       let dateAndTime = this.convertStringToDate(state.sortedSensorData[0].sensorData.timestamp)
-      this.timestamp = dateAndTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) // + ' (' + dateAndTime.toLocaleDateString() + ')'
+      this.timestamp =
+          dateAndTime.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+          + ' (' +
+          dateAndTime.toLocaleDateString([], {day: '2-digit', month: '2-digit'})
+          + ')'
     });
+  },
+
+  methods: {
+    updateSensorData24() {
+      let now = new Date()
+      let begin = this.formatDate(now, "YYYYMMDD-hhmmss")
+      now.setHours(now.getHours() - 24)
+      let end = this.formatDate(now, "YYYYMMDD-hhmmss")
+      this.updateStore(store, begin, end)
+    }
   }
 });
 </script>
